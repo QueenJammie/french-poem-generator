6c83e44b55dfb92af5d1d5a81b2f28afa96b44f2
QueenJammie <jammie.lacc@outlook.com>
2023-12-21 16:11:01 -0500

    Update the prompt

--- src/script.js
+++ src/script.js
@@ -31,7 +31,7 @@ function handleSubmit(event) {
 
     let apiKey = "00261b686eo1ff734tce27d5a5f63915";
     let context = "You are a prestigious poem writer";
-    let prompt = `Hello, miss! I'd like you to write a poem for me and my lover boy. Please generate a poem about ${sujetInput.value} for me and my boyfriend, Ismail! Do not use gender in your sentences.`;
+    let prompt = `Please, generate a poem about ${sujetInput.value} in basic HTML! Do not use gender in your sentences and follow the user instructions`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     axios.get(apiUrl).then(submitPoem);
