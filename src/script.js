ae788f6f7209d8c9ce8b05dc396fad3ff8ff24b4
QueenJammie <jammie.lacc@outlook.com>
2023-12-21 16:35:30 -0500

    Correct bugs

--- src/script.js
+++ src/script.js
@@ -30,8 +30,8 @@ function handleSubmit(event) {
     let sujetInput = document.querySelector("#sujetInput");
 
     let apiKey = "00261b686eo1ff734tce27d5a5f63915";
-    let context = "You are a prestigious poem writer";
-    let prompt = `Please, generate a french poem about ${sujetInput.value} in basic HTML! Do not use gender in your sentences and follow the user instructions`;
+    let context = "You are a prestigious poem writer. Do not use gender in your sentences and follow the user instructions.";
+    let prompt = `Please, generate a french poem about ${sujetInput.value} in basic HTML!`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     axios.get(apiUrl).then(submitPoem);
