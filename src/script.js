ba7870897155ff8cb70cc64d0bc8b4a7359a8459
QueenJammie <jammie.lacc@outlook.com>
2023-12-21 15:19:12 -0500

    Added the javascript layer

--- src/script.js
+++ src/script.js
@@ -2,19 +2,29 @@ function submitPoem(response) {
     new Typewriter("#wait", {
         strings: "Bonjour, j'ai entendu la fée du soleil couchant me demander de vous composer un poème, rien que pour vous!",
         autoStart: true,
-        delay: 25,
+        cursor: "",
+        delay: 15,
     });
 
     setTimeout(() => {
         new Typewriter("#patientez", {
             strings: "Veuillez patientez un instant...",
             autoStart: true,
-            delay: 110,
+            cursor: "",
+            delay: 30,
         });
+    }, 2000);
+
+    setTimeout(() => {
+        new Typewriter("#poem-field", {
+            strings: "The snow falls gently on the ground<br />A blanket of white, without a sound.",
+            autoStart: true,
+            delay: 30,
+        })
     }, 4000);
 }
 
-function handleClick(event) {
+function handleSubmit(event) {
     event.preventDefault();
 
     let apiKey = "00261b686eo1ff734tce27d5a5f63915";
@@ -25,5 +35,5 @@ function handleClick(event) {
     axios.get(apiUrl).then(submitPoem);
 }
 
-let submitButton = document.querySelector(".submit-button");
-submitButton.addEventListener("click", handleClick);
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", handleSubmit);
